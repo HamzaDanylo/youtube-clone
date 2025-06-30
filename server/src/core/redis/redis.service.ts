f61dbd15a02112f20ea1b69080f01ac537e73407
Danya <danagamza@gmail.com>
2025-07-01 00:58:40 +0300

    Fix Redis session storage issues: switched from ioredis to official redis client

--- server/src/core/redis/redis.service.ts
+++ server/src/core/redis/redis.service.ts
@@ -1,12 +1,16 @@
 import { Injectable } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
-import Redis from 'ioredis'
- 
+import { createClient, RedisClientType } from 'redis';
 
 @Injectable()
-export class RedisService extends Redis{
-  public constructor(private readonly configService: ConfigService){
-    super(configService.getOrThrow<string>('REDIS_URI'))
+export class RedisService {
+  public client: RedisClientType;
+
+  constructor(private readonly configService: ConfigService) {
+    this.client = createClient({
+      url: this.configService.getOrThrow<string>('REDIS_URI'),
+    });
+
+    this.client.connect().catch(console.error);
   }
 }
-
--- server/src/main.ts
+++ server/src/main.ts
@@ -27,22 +27,22 @@ async function bootstrap() {
     resave: false,
     saveUninitialized: false,
     cookie: {
-      // domain: config.getOrThrow<string>('SESSION_DOMAIN'),
+      // domain: config.getOrThrow<string>('SESSION_DOMAIN'), // можна закоментувати для локалки
       maxAge: ms(config.getOrThrow<StringValue>('SESSION_MAX_AGE')),
       httpOnly: parseBoolean(config.getOrThrow<string>('SESSION_HTTP_ONLY')),
       secure: parseBoolean(config.getOrThrow<string>('SESSION_SECURE')),
-      sameSite: 'lax'
+      sameSite: 'lax',
     },
-    store:  new RedisStore({
-      client: redis,
+    store: new RedisStore({
+      client: redis.client,
       prefix: config.getOrThrow<string>('SESSION_FOLDER'),
       serializer: {
-      stringify: JSON.stringify,
-      parse: JSON.parse
-    }
-    })
-    
-  }))
+        stringify: JSON.stringify,
+        parse: JSON.parse,
+      },
+    }),
+  }));
+
   app.enableCors({
     origin: config.getOrThrow<string>('ALLOWED_ORIGIN'),
     credentials: true,
